d2bdba266f47f1def4094d472ebfd1ba43a09e89
marcosgontijo <marcoseduardogontijo@gmail.com>
2025-03-02 16:32:30 -0300

    configurando producers nas classes

---
+++ base_files/order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/producer/SagaProducer.java
@@ -0,0 +1,30 @@
+package br.com.microservices.orchestrated.orderservice.core.producer;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SagaProducer {
+
+    private final KafkaTemplate<String, String> kafaKafkaTemplate;
+
+
+    @Value("${spring.kafka.topic.start-saga}")
+    private String startSagaTopic;
+
+    public void sendEvent(String payload) {
+        try {
+            log.info("Sending event to topic {} with data {}", startSagaTopic, payload);
+            kafaKafkaTemplate.send(startSagaTopic, payload);
+        } catch (Exception ex) {
+            log.error("Error trying to send data to topic {} with data {}", startSagaTopic, payload, ex);
+        }
+    }
+
+}
